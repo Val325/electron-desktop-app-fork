a466edd06adfe731c59c9e4b61ece36453ae37ea
darknil <jrmlelantus@gmail.com>
2024-04-12 22:56:57 +0200

    create auth page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { HashRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom'
+import './App.css'
+
+function Main() {
+  return (
+    <div>
+      <h1>Main</h1>
+    </div>
+  )
+}
+
+function Auth() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = () => {
+    // Здесь можно добавить логику для проверки логина и пароля
+    console.log('Username:', username)
+    console.log('Password:', password)
+  }
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Application Name</h1>
+        <form onSubmit={handleLogin}>
+          <div>
+            <input
+              type='text'
+              id='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder='Username'
+            />
+          </div>
+          <div>
+            <input
+              type='password'
+              id='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Password'
+            />
+          </div>
+          <button type='submit'>Login</button>
+        </form>
+      </header>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <HashRouter>
+          <Routes>
+            <Route path='/' element={<Auth />} />
+          </Routes>
+        </HashRouter>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
