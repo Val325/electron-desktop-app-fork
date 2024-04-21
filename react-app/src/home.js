34ab7534f3d92dfa1f19f1b77d725f8a905f55cd
HugginLover <valera777pushkin777@gmail.com>
2024-04-21 18:58:14 +0300

    add clicks

--- react-app/src/home.js
+++ react-app/src/home.js
@@ -1,19 +1,35 @@
 function Home() {
+  const Tasks = () => {
+    console.log("Tasks!")
+  };
+
+  const Staff = () => {
+    console.log("Staff!")
+  };
+
+  const TasksCreate = () => {
+    console.log("TasksCreate!")
+  };
+
+  const Create = () => {
+    console.log("Create!")
+  };
+
   return (
     <div>
       <h5>home page</h5>
       <div className='Home-main'>
         <div className='Home-left'>
             <div>Home left</div>
-            <div>Tasks</div>
-            <div>Staff</div>
+            <div><button onClick={Tasks}>Tasks</button></div>
+            <div><button onClick={Staff}>Staff</button></div>
             
         </div>
         <div className='Home-right'>
             <div>Home right</div>
             <div className='Tasks-staff-create'>
-                <div>Tasks</div> 
-                <div>Create</div>
+                <div><button onClick={TasksCreate}>Tasks</button></div> 
+                <div><button onClick={Create}>Create</button></div>
                 
             </div>
             <div className='Task-white'>White</div>
@@ -22,4 +38,5 @@ function Home() {
     </div>
   )
 }
+
 export default Home
