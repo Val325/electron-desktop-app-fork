bf1c6997e8739ef1ce370c8b31b53ab0b2096762
HugginLover <valera777pushkin777@gmail.com>
2024-04-21 18:32:13 +0300

    add new layout

--- react-app/src/home.js
+++ react-app/src/home.js
@@ -1,7 +1,24 @@
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h5>home page</h5>
+      <div className='Home-main'>
+        <div className='Home-left'>
+            <div>Home left</div>
+            <div>Tasks</div>
+            <div>Staff</div>
+            
+        </div>
+        <div className='Home-right'>
+            <div>Home right</div>
+            <div className='Tasks-staff-create'>
+                <div>Tasks</div> 
+                <div>Create</div>
+                
+            </div>
+            <div className='Task-white'>White</div>
+        </div>
+      </div>
     </div>
   )
 }
